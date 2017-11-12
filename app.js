69ae75cddd15699089b63f98773c1d1ef436e9cc
Salvador Guzman <guzmansalv@gmail.com>
2017-11-12 07:41:49 -0800

    commit project files

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(logger('combined'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+module.exports = app;
